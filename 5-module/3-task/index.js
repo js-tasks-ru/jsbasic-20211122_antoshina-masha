e7b2eae1332267ecc881f04da766a58ead72a0ed
somename <some@email.com>
2021-12-26 00:08:11 +0300

    intermediate commit

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,41 @@
 function initCarousel() {
-  // ваш код...
+  let carousel = document.getElementsByClassName("carousel__slide");
+  let rightButton = document.getElementsByClassName("carousel__arrow_right")[0];
+  let leftButton = document.getElementsByClassName("carousel__arrow_left")[0];
+  let index = 0;
+  let currentImg = carousel[index];
+  rightButton.addEventListener('click', rightListener);
+  leftButton.addEventListener('click', leftListener);
+  leftButton.style.display = 'none';
+
+  function rightListener() {
+    index++
+    leftButton.style.display = 'flex'
+    if (index > carousel.length - 1 || index == carousel.length - 1) {
+      index = carousel.length - 1;
+      rightButton.style.display = 'none';
+    }
+
+    currentImg = carousel[index];
+    toggleImg();
+  }
+
+  function leftListener() {
+    index--
+    rightButton.style.display = 'flex'
+    if (index < 0 || index == 0) {
+      index = 0;
+      leftButton.style.display = 'none';
+    }
+    currentImg = carousel[index];
+    toggleImg();
+  }
+
+  function toggleImg() {
+    currentImg.hidden = false;
+    let filteredCarousel = Array.prototype.slice.call(carousel).filter(img => img != currentImg);
+    filteredCarousel.forEach(img => {
+      img.hidden = true;
+    });
+  }
 }
