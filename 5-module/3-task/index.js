c72c608018d2ce41be05cae493d753eb63aa9dd7
somename <some@email.com>
2021-12-29 23:25:02 +0300

    fix

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,5 +1,8 @@
 function initCarousel() {
   let carousel = document.getElementsByClassName("carousel__slide");
+  let carouselInner = document.getElementsByClassName("carousel__inner");
+  let allSlides = carouselInner[0]
+
   let rightButton = document.getElementsByClassName("carousel__arrow_right")[0];
   let leftButton = document.getElementsByClassName("carousel__arrow_left")[0];
   let index = 0;
@@ -7,28 +10,36 @@ function initCarousel() {
   rightButton.addEventListener('click', rightListener);
   leftButton.addEventListener('click', leftListener);
   leftButton.style.display = 'none';
+  let delta = carouselInner[0].offsetWidth
+
+  let initialPXValue = 0;
+
 
   function rightListener() {
     index++
+    initialPXValue -= delta;
     leftButton.style.display = 'flex'
     if (index > carousel.length - 1 || index == carousel.length - 1) {
       index = carousel.length - 1;
       rightButton.style.display = 'none';
     }
+    carouselInner[0].style.transform = "translateX(" + initialPXValue + "px)"
 
-    currentImg = carousel[index];
-    toggleImg();
+    // currentImg = carousel[index];
+    // toggleImg();
   }
 
   function leftListener() {
     index--
+    initialPXValue += delta;
     rightButton.style.display = 'flex'
     if (index < 0 || index == 0) {
       index = 0;
       leftButton.style.display = 'none';
     }
-    currentImg = carousel[index];
-    toggleImg();
+    carouselInner[0].style.transform = "translateX(" + initialPXValue + "px)"
+    // currentImg = carousel[index];
+    // toggleImg();
   }
 
   function toggleImg() {
